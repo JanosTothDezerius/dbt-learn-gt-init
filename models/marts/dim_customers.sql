605a097bbcfa731b200f3cd9f5091d88531303cd
Janos Toth <janos.toth@dezerius.com>
2026-09-10 16:57:32 +0200

    add payment data model

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -10,18 +10,27 @@ orders as (
 
 ),
 
+orders_value as (
+
+    select * from {{ ref('fct_orders') }}
+
+),
+
 customer_orders as (
 
     select
-        customer_id,
+        o.customer_id,
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        min(o.order_date) as first_order_date,
+        max(o.order_date) as most_recent_order_date,
+        count(o.order_id) as number_of_orders,
+        sum(ov.amount) as lifetime_value
 
-    from orders
+    from orders as o
 
-    group by customer_id
+    left join orders_value as ov on o.order_id = ov.order_id
+
+    group by o.customer_id
 
 ),
 
@@ -34,7 +43,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value as lifetime_value
 
     from customers
 
---
+++ models/staging/stripe/stg_jaffle_shop_payments.sql
@@ -0,0 +1,9 @@
+select
+    ID as payment_id,
+    ORDERID as order_id,
+    PAYMENTMETHOD as payment_method,
+    STATUS as status,
+    AMOUNT / 100.0 as amount, -- stored in cents, convert to dollars
+    CREATED as created_at
+
+from raw.payments
